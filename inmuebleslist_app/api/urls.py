1f41631a76bc1331c3cccc32299f75bb3b5b5846
David Amador <54488581+dsamador@users.noreply.github.com>
2022-02-03 09:00:48 -0500

    Organizando las urls

--- inmuebleslist_app/api/urls.py
+++ inmuebleslist_app/api/urls.py
@@ -11,11 +11,12 @@ from inmuebleslist_app.api.views import (
 )
 
 urlpatterns = [
-    path('list/', InmuebleListAV.as_view(), name='inmueble-list'),
-    path('<int:pk>/', InmuebleDetalleAV.as_view(), name='inmueble-detail'),
+    path('inmueble/list/', InmuebleListAV.as_view(), name='inmueble-list'),
+    path('inmueble/<int:pk>/', InmuebleDetalleAV.as_view(), name='inmueble-detail'),
+    
     path('empresa/', EmpresaAV.as_view(), name='empresa'),    
     path('empresa/<int:pk>', EmpresaDetalleAV.as_view(), name='empresa-detail'),    
     
-    path('comentario/', ComentarioList.as_view(), name='comentario-list'),    
-    path('comentario/<int:pk>', ComentarioDetail.as_view(), name='comentario-detail'),
+    path('inmueble/<int:pk>/comentario/', InmuebleDetalleAV.as_view(), name='comentario-list'),    
+    path('inmueble/comentario/<int:pk>', ComentarioDetail.as_view(), name='comentario-detail'),
 ]
