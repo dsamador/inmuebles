8fa0b1004db5f5fcc1921051d8a882e4b540f65c
David Amador <54488581+dsamador@users.noreply.github.com>
2022-02-13 10:58:42 -0500

    Login, register and migrations

--- inmuebles/settings.py
+++ inmuebles/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'inmuebles.wsgi.application'
 
+AUTH_USER_MODEL = 'user_app.Account'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
--- inmuebleslist_app/models.py
+++ inmuebleslist_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import User
+from user_app.models import Account
 # Create your models here.
 
 class Empresa(models.Model):
@@ -32,7 +33,7 @@ class Inmueble(models.Model):
 
 class Comentario(models.Model):
     
-    comentario_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comentario_user = models.ForeignKey(Account, on_delete=models.CASCADE)
     calificacion    = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     texto           = models.CharField(max_length=200, null=True)
     inmueble        = models.ForeignKey(Inmueble, on_delete=models.CASCADE, related_name="comentarios")
