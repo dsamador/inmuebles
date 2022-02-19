c3fe08c623d69fdfc4878ab57f3799d02e171be5
David Amador <54488581+dsamador@users.noreply.github.com>
2022-02-19 04:36:10 -0500

    Ready to go, complete configs

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,9 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+/* FIREBASE */
+
+import {AngularFireModule} from '@angular/fire/compat';
+import {AngularFireStorageModule} from '@angular/fire/compat/storage';
+import {AngularFirestoreModule} from '@angular/fire/compat/firestore';
+import {AngularFireAuthModule} from '@angular/fire/compat/auth';
+
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+import {getAuth, provideAuth} from '@angular/fire/auth';
+import {getStorage, provideStorage} from '@angular/fire/storage';
+
+/* END FIREBASE */
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsuarioComponent } from './usuario/usuario.component';
+import { environment } from '@src/environments/environment';
 
 
 @NgModule({
@@ -14,6 +29,15 @@ import { UsuarioComponent } from './usuario/usuario.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+    ,provideFirebaseApp(
+      ()=>initializeApp(environment.firebase.config))
+    ,provideFirestore(()=>getFirestore())
+    ,provideStorage(()=>getStorage())
+    ,provideAuth(()=>getAuth())
+    ,AngularFireModule.initializeApp(environment.firebase.config)
+    ,AngularFireStorageModule
+    ,AngularFirestoreModule
+    ,AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
