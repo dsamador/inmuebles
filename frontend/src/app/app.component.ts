ac95d865b1590a5194cb46b1e7539ad15d53b189
David Amador <54488581+dsamador@users.noreply.github.com>
2022-02-19 04:54:07 -0500

    test firestore success

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-import { environment } from '@src/environments/environment';
-
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'mi-web-app';
+  /**
+   *
+   */
+  constructor(private fs : AngularFirestore) {
+
+  }
+
+  ngOnInit(): void {
+      this.fs.collection('test').stateChanges().subscribe(
+        personas => {console.log(personas.map(
+          p=>p.payload.doc.data()
+        ))}
+      );
+  }
 }
