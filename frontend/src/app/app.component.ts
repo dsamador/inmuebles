e7b9977891a7460f637856b73f7121dc46efdd48
David Amador <54488581+dsamador@users.noreply.github.com>
2022-02-21 15:06:34 -0500

    starting again the frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
   title = 'mi-web-app';
