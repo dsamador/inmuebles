b64960dce9f01efc13c88677a66f97251284c35d
David Amador <54488581+dsamador@users.noreply.github.com>
2022-02-21 16:56:42 -0500

    configs

--- client-inmueble-app/src/app/app.module.ts
+++ client-inmueble-app/src/app/app.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+/* FIREBASE */
+
+import {AngularFireModule} from '@angular/fire/compat';
+import {AngularFireStorageModule} from '@angular/fire/compat/storage';
+import {AngularFirestoreModule} from '@angular/fire/compat/firestore';
+import {AngularFireAuthModule} from '@angular/fire/compat/auth';
+
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+import {getAuth, provideAuth} from '@angular/fire/auth';
+import {getStorage, provideStorage} from '@angular/fire/storage';
+
+/* END FIREBASE */
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '@src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -11,6 +26,15 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+    ,provideFirebaseApp(
+      ()=>initializeApp(environment.firebase.config))
+    ,provideFirestore(()=>getFirestore())
+    ,provideStorage(()=>getStorage())
+    ,provideAuth(()=>getAuth())
+    ,AngularFireModule.initializeApp(environment.firebase.config)
+    ,AngularFireStorageModule
+    ,AngularFirestoreModule
+    ,AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client-inmueble-app/src/environments/environment.dev.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false
+  ,name: 'dev'
+  ,firebase: {
+      config : {
+        apiKey: "AIzaSyBPOJcL3vCYB4rvWyo8QuzsV-fHbe3FIUs",
+        authDomain: "inmuebles-app-200a3.firebaseapp.com",
+        projectId: "inmuebles-app-200a3",
+        storageBucket: "inmuebles-app-200a3.appspot.com",
+        messagingSenderId: "506500481558",
+        appId: "1:506500481558:web:c114b2b7951e89b67feb37"
+    }
+  }
+};
