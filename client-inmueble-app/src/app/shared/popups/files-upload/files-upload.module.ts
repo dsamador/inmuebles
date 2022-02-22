d099ce656890f35a4a88e11ad08b0449ae6fa400
David Amador <54488581+dsamador@users.noreply.github.com>
2022-02-22 09:50:07 -0500

    drag and drop directive

--- client-inmueble-app/src/app/shared/popups/files-upload/files-upload.module.ts
+++ client-inmueble-app/src/app/shared/popups/files-upload/files-upload.module.ts
@@ -4,11 +4,13 @@ import { FilesUploadComponent } from './files-upload.component';
 import { FilesUploadDirective } from './files-upload.directive';
 
 import { MatDialogModule } from '@angular/material/dialog';
+import { DropZoneDirective } from './directives/drop-zone/drop-zone.directive';
 
 @NgModule({
   declarations: [
     FilesUploadComponent,
-    FilesUploadDirective
+    FilesUploadDirective,
+    DropZoneDirective
   ],
   imports: [
     CommonModule,
