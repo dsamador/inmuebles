7a504409f25d6d554d65691edc72e8e7f624bd48
David Amador <54488581+dsamador@users.noreply.github.com>
2022-03-01 20:18:20 -0500

    static pages

--- client-inmueble-app/src/app/app-routing.module.ts
+++ client-inmueble-app/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: ''
+    ,children: [
+      {
+        path: 'static'
+        ,loadChildren: () => import('./pages/static/static.module').then(m=>m.StaticModule)
+      },
+      {
+        path:''
+        ,pathMatch: 'full'
+        ,redirectTo: 'static/welcome'
+      }
+    ]
+  },
+  {//ruta para ruta no encontrada
+    path: '**'
+    ,pathMatch: 'full'
+    ,redirectTo: 'static/welcome'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
