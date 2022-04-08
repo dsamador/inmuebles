ea9599ef85599a1309e3ed28b84488f2f4d493c3
David Amador <54488581+dsamador@users.noreply.github.com>
2022-04-08 17:46:51 -0500

    Login component

--- client-inmueble-app/src/app/pages/auth/pages/login/login.module.ts
+++ client-inmueble-app/src/app/pages/auth/pages/login/login.module.ts
@@ -4,6 +4,14 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
+/* Material */
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 
 @NgModule({
   declarations: [
@@ -11,7 +19,14 @@ import { LoginComponent } from './login.component';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    /* Material */
+    MatFormFieldModule,
+    MatButtonModule,
+    FormsModule,
+    MatIconModule,
+    MatCardModule,
+    FlexLayoutModule
   ]
 })
 export class LoginModule { }
